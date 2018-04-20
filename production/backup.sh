10683f79a09f69700ab1c2ef8bb2e2d59f28eb8c
Nolan Darilek <nolan@thewordnerd.info>
2018-04-20 17:24:50 +0000

    Commit initial backup infrastructure.

---
+++ production/backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/loconomics
+source ./.env
+docker exec loconomics_db_1 mysqldump -u suitecrm --password=$MYSQL_PASSWORD suitecrm |
+  docker run -i --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -e AZURE_ACCOUNT_NAME=$AZURE_ACCOUNT_NAME -e AZURE_ACCOUNT_KEY=$AZURE_ACCOUNT_KEY restic/restic -r azure:mysql:/ backup --stdin
