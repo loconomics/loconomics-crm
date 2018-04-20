200efc1f93290f30185f3299a62ab6a8f270ef80
Nolan Darilek <nolan@thewordnerd.info>
2018-04-20 17:48:08 +0000

    Use RESTIC_REPOSITORY for easier configuration and administration.

--- production/backup.sh
+++ production/backup.sh
@@ -3,4 +3,4 @@
 cd /home/loconomics
 source ./.env
 docker exec loconomics_db_1 mysqldump -u suitecrm --password=$MYSQL_PASSWORD suitecrm |
-  docker run -i --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -e AZURE_ACCOUNT_NAME=$AZURE_ACCOUNT_NAME -e AZURE_ACCOUNT_KEY=$AZURE_ACCOUNT_KEY restic/restic -r azure:mysql:/ backup --stdin
+  docker run -i --rm -e RESTIC_REPOSITORY=$RESTIC_REPOSITORY -e RESTIC_PASSWORD=$RESTIC_PASSWORD -e AZURE_ACCOUNT_NAME=$AZURE_ACCOUNT_NAME -e AZURE_ACCOUNT_KEY=$AZURE_ACCOUNT_KEY restic/restic backup --stdin
